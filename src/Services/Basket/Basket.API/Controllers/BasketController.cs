6db97d5d24ac1dec56ea83440a53f93877b6905c
seyhmusozbek <seyhmusozbek@gmail.com>
2022-06-15 13:12:36 +0300

    finished discount grpc dockerization

--- src/Services/Basket/Basket.API/Controllers/BasketController.cs
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -1,4 +1,5 @@
 ﻿using Basket.API.Entities;
+using Basket.API.GrpcServices;
 using Basket.API.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using System;
@@ -12,10 +13,12 @@ namespace Basket.API.Controllers
     public class BasketController : ControllerBase
     {
         private readonly IBasketRepository _repository;
+        private readonly DiscountGrpcService _grpcClient;
 
-        public BasketController(IBasketRepository repository)
+        public BasketController(IBasketRepository repository, DiscountGrpcService grpcClient)
         {
-            _repository = repository ?? throw new ArgumentNullException(nameof(repository)); ;
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+            _grpcClient = grpcClient ?? throw new ArgumentNullException(nameof(grpcClient));
         }
 
         [HttpGet("{userName}",Name ="GetBasket")]
@@ -30,6 +33,13 @@ namespace Basket.API.Controllers
         [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK)]
         public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
         {
+            // TODO : Communicate with Discount.Grpc
+            // and Calculate latest prices of product
+            foreach (var item in basket.Items)
+            {
+                var coupon = await _grpcClient.GetDiscount(item.ProductName);
+                item.Price -= coupon.Amount;
+            }
             return Ok(await _repository.UpdateBasket(basket));
         }
 
