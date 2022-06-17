aefc39b5f24904a3c282b0933323632842c6b171
seyhmusozbek <seyhmusozbek@gmail.com>
2022-06-17 15:20:08 +0300

    added ordering api

---
+++ src/Services/Ordering/Ordering.Infrastructure/Persistence/OrderContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Ordering.Domain.Common;
+using Ordering.Domain.Entities;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ordering.Infrastructure.Persistence
+{
+    public class OrderContext : DbContext
+    {
+        public OrderContext(DbContextOptions<OrderContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Order> Orders { get; set; }
+
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            foreach (var entry in ChangeTracker.Entries<EntityBase>())
+            {
+                switch (entry.State)
+                {
+                    case EntityState.Added:
+                        entry.Entity.CreatedAt = DateTime.Now;
+                        entry.Entity.CreatedBy = "swn";
+                        break;
+                    case EntityState.Modified:
+                        entry.Entity.ModifiedAt= DateTime.Now;
+                        entry.Entity.ModifiedBy = "swn";
+                        break;
+                }
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
